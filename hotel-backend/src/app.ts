a2268d1afc386da55e13806308628d8ed8be6699
¨FrancoGatti¨ <fgattidelaconcepcion@gmail.com>
2025-12-16 22:51:41 -0300

    add routes to guest in app.ts

--- hotel-backend/src/app.ts
+++ hotel-backend/src/app.ts
@@ -11,21 +11,19 @@ import dashboardRoutes from "./routes/dashboard.routes";
 
 const app = express();
 
-// Middlewares globales
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(morgan("dev"));
 
-// Forzar codificación UTF-8
 app.use((req, res, next) => {
   res.setHeader("Content-Type", "application/json; charset=utf-8");
   next();
 });
 
-// Rutas principales
-app.use("/api", mainRouter);
-
-
+// ✅ Primero dashboard
 app.use("/api/dashboard", dashboardRoutes);
+
+// ✅ Luego el resto
+app.use("/api", mainRouter);
 
 export default app;
